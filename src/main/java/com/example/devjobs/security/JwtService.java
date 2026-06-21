6d5ea15f7579fc844b97a45aa552a1a47edde66c
Thomy <th.martinezb@duocuc.cl>
2026-06-21 04:59:28 -0400

    Service mas anotaciones.

---
+++ src/main/java/com/example/devjobs/security/JwtService.java
@@ -0,0 +1,70 @@
+package com.example.devjobs.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    public String getToken(String email) {
+        return getToken(new HashMap<>(), email);
+    }
+
+    public String getToken(Map<String, Object> extraClaims, String email) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private SecretKey getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+    public String getUsernameFromToken(String token) {
+        return getClaim(token, Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token) {
+        return !isTokenExpired(token);
+    }
+
+    private Claims getAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public <T> T getClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims=getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Date getExpiration(String token){
+        return getClaim(token, Claims::getExpiration);
+    }
+    private boolean isTokenExpired(String token){
+        return getExpiration(token).before(new Date());
+    }
+}
---
+++ src/main/java/com/example/devjobs/service/AuthService.java
@@ -0,0 +1,82 @@
+package com.example.devjobs.service;
+
+import com.example.devjobs.dto.request.LoginRequest;
+import com.example.devjobs.dto.request.RegisterRequest;
+import com.example.devjobs.dto.response.AuthResponse;
+import com.example.devjobs.exception.DuplicateEmailException;
+import com.example.devjobs.exception.ResourceNotFoundException;
+import com.example.devjobs.exception.UnauthorizedActionException;
+import com.example.devjobs.model.User;
+import com.example.devjobs.repository.IUserRepository;
+import com.example.devjobs.security.JwtService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final IUserRepository iUserRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public AuthResponse register(RegisterRequest request) {
+        log.info("Iniciando registro de usuario con correo: {}", request.getEmail());
+        if (iUserRepository.findByEmail(request.getEmail()).isPresent()){
+            log.warn("Usuario con email {} ya existe.", request.getEmail());
+            throw new DuplicateEmailException("Email ya existe.");
+        }
+        User user = User.builder()
+                .email(request.getEmail())
+                .passwordHash(passwordEncoder.encode(request.getPassword()))
+                .fullName(request.getFullName())
+                .role(request.getRole())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+        iUserRepository.save(user);
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", request.getRole());
+        claims.put("email", request.getEmail());
+
+        log.info("Generando token JWT para el usuario: {}", request.getEmail());
+        String token = jwtService.getToken(claims, request.getEmail());
+        log.info("Registro completado correctamente para el usuario: {}", request.getEmail());
+        return AuthResponse.builder()
+                .token(token)
+                .email(request.getEmail())
+                .fullName(request.getFullName())
+                .role(request.getRole())
+                .build();
+    }
+    public AuthResponse login(LoginRequest request){
+        User user = iUserRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> {
+                    log.warn("Usuario con email: {} no existe.", request.getEmail());
+                    return new ResourceNotFoundException("Email no existe.");
+                });
+        if(!passwordEncoder.matches(request.getPassword(), user.getPasswordHash())){
+            log.warn("Login fallido: contraseña incorrecta | email: {}", request.getEmail());
+            throw new UnauthorizedActionException("Contraseña incorrecta.");
+        }
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", user.getRole());
+        claims.put("email", user.getEmail());
+
+        log.info("Generando token para el usuario: {}", user.getEmail());
+        String token = jwtService.getToken(claims, user.getEmail());
+        log.info("Login exitoso para el usuario: {}", user.getEmail());
+        return AuthResponse.builder().token(token).build();
+
+    }
+}
