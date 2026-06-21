5facfb1d98796629e6c6a0b582cfc7d335ccd68e
Thomy <th.martinezb@duocuc.cl>
2026-06-21 18:40:26 -0400

    Services con reglas de negocio implementadas.

--- src/main/java/com/example/devjobs/service/CompanyService.java
+++ src/main/java/com/example/devjobs/service/CompanyService.java
@@ -14,8 +14,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
