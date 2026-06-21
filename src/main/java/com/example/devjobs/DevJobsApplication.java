f7b76f4580bb477337fc423fe84a89d453dfdd91
Thomy <th.martinezb@duocuc.cl>
2026-06-21 00:22:39 -0400

    Base Project

---
+++ src/main/java/com/example/devjobs/DevJobsApplication.java
@@ -0,0 +1,13 @@
+package com.example.devjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevJobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevJobsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devjobs/DevJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevJobsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
