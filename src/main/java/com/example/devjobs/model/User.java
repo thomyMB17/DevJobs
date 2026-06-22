fea5ef8d58d0970afe96bfc5ee00c7a3d5f5316d
Thomy <th.martinezb@duocuc.cl>
2026-06-22 17:34:59 -0400

    Roles en GetAuthorities

--- src/main/java/com/example/devjobs/model/User.java
+++ src/main/java/com/example/devjobs/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
@@ -44,7 +45,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
     }
 
     @Override
