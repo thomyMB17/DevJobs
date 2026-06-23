82e4cfe1dc58b1ac5e184e324a83aaddadd75778
Thomy <th.martinezb@duocuc.cl>
2026-06-22 22:50:46 -0400

    Peticiones actualizadas

--- src/main/java/com/example/devjobs/repository/IApplicationRepository.java
+++ src/main/java/com/example/devjobs/repository/IApplicationRepository.java
@@ -1,16 +1,16 @@
 package com.example.devjobs.repository;
 
 import com.example.devjobs.model.Application;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface IApplicationRepository extends JpaRepository<Application, Long> {
     boolean existsByCandidateIdAndJobPostingId(Long userId, Long jobId);
 
-    List<Application> findByCandidate_Id(Long id);
+    Page<Application> findByCandidate_Id(Long id, Pageable pageable);
 
-    List<Application> findByJobPosting_Id(Long id);
+    Page<Application> findByJobPosting_Id(Long id, Pageable pageable);
 }
